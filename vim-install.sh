249f13299132e63d2809d7b7b48127227fb56fa0
Philippe Schottey <philippe@Philippes-MacBook.local>
2018-03-10 09:28:31 +0100

    Remember plugins to install

---
+++ vim-install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+
