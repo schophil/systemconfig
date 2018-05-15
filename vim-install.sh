f03fb5b29aebd796e891813c1196c13b35a05c72
Philippe Schottey <schophil@icloud.com>
2018-05-15 15:28:08 +0200

    add fugitive

--- vim-install.sh
+++ vim-install.sh
@@ -5,4 +5,4 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
-
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
