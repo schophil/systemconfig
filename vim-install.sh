8b13a67a0003a4f41657ba1d66487ab9f48faa69
Philippe Schottey <schophil@icloud.com>
2018-05-16 14:40:59 +0200

    Added airline themes

--- vim-install.sh
+++ vim-install.sh
@@ -6,3 +6,5 @@ git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcomm
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
