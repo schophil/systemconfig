19c23099b14dbb8215caf83d0ec797e616c81e29
Philippe Schottey <philippe@Philippes-MacBook-Pro.local>
2018-05-28 08:08:04 +0200

    Create bundle dir

--- vim-install.sh
+++ vim-install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
