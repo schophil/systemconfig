31480b520c3d032ded12199a2b0d4eaab4b8edb7
Philippe Schottey <philippe@Philippes-MacBook-Pro.local>
2018-05-28 08:07:43 +0200

    test the folders to add to the path

--- fish/config.fish
+++ fish/config.fish
@@ -1,18 +1,29 @@
 set -x JAVA_HOME "temp"
-set -x PATH ~/.npm-global/bin $PATH
 
-function setpath
-  set -x PATH $JAVA_HOME/bin ~/tools/apache-ant/bin /opt/local/bin /opt/local/sbin $PATH
+if test -d ~/.npm-global/bin
+	set -x PATH ~/.npm-global/bin $PATH
+end
+
+if test -d /opt/local/bin
+	set -x PATH /opt/local/bin $PATH
+end
+
+if test -d /opt/local/sbin
+	set -x PATH /opt/local/sbin $PATH
+end
+
+if test -d ~/tools/apache-ant/bin
+	set -x PATH /~/tools/apache-ant/bin $PATH
 end
 
 function @java8
-  set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home
-  set -x PATH $JAVA_HOME/bin ~/tools/apache-ant/bin /opt/local/bin /opt/local/sbin $PATH
+  set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_171.jdk/Contents/Home
+  set -x PATH $JAVA_HOME/bin $PATH
 end
 
 function @java9
   set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk-9.jdk/Contents/Home
-  set -x PATH $JAVA_HOME/bin ~/tools/apache-ant/bin /opt/local/bin /opt/local/sbin $PATH
+  set -x PATH $JAVA_HOME/bin $PATH
 end
 
 @java8
