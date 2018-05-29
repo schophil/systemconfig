dd1610fbadb2f08bb194228e7a41591cd181859a
Philippe Schottey <schophil@icloud.com>
2018-05-29 07:26:00 +0200

    Installing pathogen and color theme

--- vim-install.sh
+++ vim-install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Installing pathogen
+echo Installing pathogen...
+mkdir -p ~/.vim/autoload
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Installing the plugins
+echo Installing bundles...
 mkdir -p ~/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
@@ -9,4 +16,12 @@ git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 git clone https://github.com/dag/vim-fish.git ~/.vim/bundle/vim-fish
+git clone https://github.com/editorconfig/editorconfig-vim.git ~/.vim/bundle/editorconfig-vim
+
+# Installing the color theme
+echo Installing color theme...
+mkdir -p ~/.vim/colors
+git clone https://github.com/sickill/vim-monokai.git /tmp/vim-monokai
+cp /tmp/vim-monokai/colors/monokai.vim ~/.vim/colors
+rm -rf /tmp/vim-monokai
 
