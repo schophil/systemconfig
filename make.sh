b48fbd08ce4b0185f3abe5d99860237bf5e9d81d
Philippe Schottey <schophil@icloud.com>
2018-07-22 22:56:02 +0200

    Prepare work for snippets

--- make.sh
+++ make.sh
@@ -3,8 +3,10 @@
 base=`pwd`
 
 mkdir -p ~/.config/fish
+mkdir -p ~/.vim
 ln -s -f $base/fish/config.fish ~/.config/fish/config.fish 
 ln -s -f $base/fish/functions ~/.config/fish/functions 
+ln -s -f $base/snippets ~/.vim/snippets
 
 for file in dotfiles/*
 do
