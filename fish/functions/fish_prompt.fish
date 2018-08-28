a1c16714b9f65e51f773446a82ade728c0e0a44a
Philippe Schottey <schophil@icloud.com>
2018-08-28 09:31:11 +0200

    Improved prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,14 @@
 function fish_prompt
   # Collect git information of the current directory
-  set gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
+  set -l gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
 
   # Main
-  echo -n (set_color cyan)(prompt_pwd) (set_color red)'❯'(set_color yellow)'❯'
-  test $status = 0
-  and echo -n (set_color yellow)$gitinfo
-  echo -n (set_color green)'❯ '
+  echo -n (set_color cyan)(prompt_pwd) '❯'(set_color yellow)
+  if test -n "$gitinfo"
+    echo -n ' '$gitinfo' ❯ '
+  end
+  if test -z "$gitinfo"
+    echo -n '❯ '
+  end
+  # echo -n (set_color green)'❯ '
 end
