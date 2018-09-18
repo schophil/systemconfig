d2c9ffb4c1742f1160fa404c54ed89ae790a946a
Philippe Schottey <schophil@icloud.com>
2018-09-18 08:22:58 +0200

    Show sub environment information

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,9 +1,17 @@
 function fish_prompt
   # Collect git information of the current directory
   set -l gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
+  
+  # First set a profile name if present
+  if test -n "$sub"
+    echo -n (set_color grey)"$sub ❯"
+  end
+  if test -z "$sub"
+    echo -n (set_color grey)"❯"
+  end
 
   # Main
-  echo -n (set_color cyan)(prompt_pwd) '❯'(set_color yellow)
+  echo -n (set_color cyan)' '(prompt_pwd) '❯'(set_color yellow)
   if test -n "$gitinfo"
     echo -n ' '$gitinfo' ❯ '
   end
