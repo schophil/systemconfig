74d172b9fbb39436fc782c6d7cdfa21e31dc9fcd
Philippe Schottey <schophil@icloud.com>
2019-03-08 20:10:16 +0100

    Use () as separators

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,18 +3,18 @@ function fish_prompt
   set -g gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
   # First set a profile name if present
   if test -n "$sub"
-    echo -n (set_color grey)"$sub ❯"
+    echo -n (set_color grey)"$sub )"
   end
   if test -z "$sub"
-    echo -n (set_color grey)"❯"
+    echo -n (set_color grey)")"
   end
   # Main
-  echo -n (set_color cyan)' '(prompt_pwd) '❯'(set_color yellow)
+  echo -n (set_color cyan)' '(prompt_pwd) ')'(set_color yellow)
   if test -n "$gitinfo"
-    echo -n ' '$gitinfo' ❯ '
+    echo -n ' '$gitinfo' ) '
   end
   if test -z "$gitinfo"
-    echo -n '❯ '
+    echo -n ') '
   end
   # echo -n (set_color green)'❯ '
 end
