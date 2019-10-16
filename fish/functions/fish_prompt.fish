4487378c00c43659d5b4c83f2a04a8ec62b591c6
Philippe Schottey <schophil@icloud.com>
2019-10-16 11:49:44 +0200

    Set different symbol

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,20 +1,22 @@
 function fish_prompt
+  # set -g sym "➜"
+  set -g sym "▶"
   # Collect git information of the current directory
   set -g gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
   # First set a profile name if present
   if test -n "$sub"
-    echo -n (set_color grey)"$sub )"
+    echo -n (set_color grey)"$sub $sym"
   end
   if test -z "$sub"
-    echo -n (set_color grey)")"
+    echo -n (set_color grey)"$sym"
   end
   # Main
-  echo -n (set_color cyan)' '(prompt_pwd) ')'(set_color yellow)
+  echo -n (set_color cyan)' '(prompt_pwd) "$sym"(set_color yellow)
   if test -n "$gitinfo"
-    echo -n ' '$gitinfo' ) '
+    echo -n ' '$gitinfo" $sym "
   end
   if test -z "$gitinfo"
-    echo -n ') '
+    echo -n "$sym "
   end
   # echo -n (set_color green)'❯ '
 end
