646f6a12565a19bf14f2159fd4768172cf44a3a1
Philippe Schottey <schophil@icloud.com>
2019-10-16 14:56:36 +0200

    Improved git info

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,8 +3,7 @@ function fish_prompt
   set -g sym "▶"
   # Collect git information of the current directory
   set -g gitinfo (git status -s -b -uno ^ /dev/null | head -n 1)
-  ## don't show the first 2 # characters
-  set -g gitinfo (string sub -s 4 $gitinfo)
+  # set -g gitinfo (string sub -s 4 $gitinfo)
   # First set a profile name if present
   if test -n "$sub"
     echo -n (set_color grey)"$sub $sym"
@@ -15,6 +14,14 @@ function fish_prompt
   # Main
   echo -n (set_color cyan)' '(prompt_pwd) "$sym"(set_color yellow)
   if test -n "$gitinfo"
+    set -g gitparts (string match -e -r '## ([\w\/]+){1}(?:\.\.\.\w+\/\w+\s?)?(\[.*\])?' $gitinfo)
+    if test "3" = (count $gitparts)
+      set -g gitinfo "$gitparts[2] $gitparts[3]"
+    else if test "2" = (count $gitparts)
+      set -g gitinfo "$gitparts[2]"
+    else 
+      set -e gitinfo
+    end
     echo -n ' '$gitinfo" $sym "
   end
   if test -z "$gitinfo"
