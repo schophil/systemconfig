6ecac1fdaa0251eb23d6d3dac1fcd21d2deacb0e
Philippe Schottey <schophil@icloud.com>
2020-03-01 19:38:18 +0100

    set vim as editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set -x LANG "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
+set -x EDITOR vim
 
 set -x EXTTRA_PATHS ~/.npm-global/bin /opt/local/bin /opt/local/sbin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ~/tools/node/bin ~/bin ~/tools/idea/bin
 
