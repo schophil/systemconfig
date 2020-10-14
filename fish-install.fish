84be575d8ecc207e5fa3d50399c885148c8c1b39
Philippe Schottey <schophil@icloud.com>
2020-10-14 11:52:11 +0200

    Fish installation script

---
+++ fish-install.fish
@@ -0,0 +1,8 @@
+#!/usr/bin/env fish
+# File: fish-install.fish
+# Author: Philippe Schottey
+
+stow fish
+curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fisher add reitzig/sdkman-for-fish
+fisher add fishpkg/fish-prompt-mono
