740674350b6118cb5efd4e771035a639dea60201
Philippe Schottey <schophil@icloud.com>
2021-06-26 07:49:11 +0200

    feat: Ignore fisher plugins

--- fish-install.fish
+++ fish-install.fish
@@ -5,3 +5,10 @@
 rm -rf ~/.config/fish
 stow fish
 curl -sL https://git.io/fisher | source && fisher update
+
+fisher install jorgebucaran/fisher
+fisher install reitzig/sdkman-for-fish
+fisher install jorgebucaran/replay.fish
+fisher install jethrokuan/z
+fisher install jorgebucaran/hydro
+fisher install schophil/fish-docker
