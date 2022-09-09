7828036445cf16326ea168b375f1f0cb68d45ae1
Philippe Schottey <schophil@icloud.com>
2022-09-09 10:09:50 +0200

    use oh my posh

--- zsh/custom/settings.zsh
+++ zsh/custom/settings.zsh
@@ -24,4 +24,4 @@ fi
 
 export M2_HOME=/home/schophil/.sdkman/candidates/maven/current
 
-eval "$(oh-my-posh init zsh --config '/opt/local/share/oh-my-posh/themes/paradox.omp.json')"
+eval "$(oh-my-posh init zsh --config '/opt/local/share/oh-my-posh/themes/stelbent-compact.minimal.omp.json')"
