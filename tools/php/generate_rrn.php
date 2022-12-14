e9485f4018358fd8bc250025d6be3735f6bd8887
Philippe Schottey <schophil@icloud.com>
2022-12-14 07:26:11 +0100

    feat: extra info

--- tools/php/generate_rrn.php
+++ tools/php/generate_rrn.php
@@ -3,9 +3,10 @@
 class GeneratedRRN {
     public $rrn;
     public $birthDate;
+    public $gender;
     public function __toString()
     {
-        return "{$this->rrn}: {$this->birthDate}";
+        return "{$this->rrn}: {$this->birthDate} - {$this->gender}";
     }
 }
 
@@ -14,12 +15,14 @@ function generateRRN(): GeneratedRRN
     $year = random_int(60, 99);
     $month = str_pad(random_int(1, 12), 2, '0', STR_PAD_LEFT);
     $day = str_pad(random_int(1, 30), 2, '0', STR_PAD_LEFT);
-    $seq = str_pad(random_int(1, 999), 3, '0', STR_PAD_LEFT);
+    $seqNbr = random_int(1, 999);
+    $seq = str_pad($seqNbr, 3, '0', STR_PAD_LEFT);
     $base = "{$year}{$month}{$day}{$seq}";
     $remainder = 97 - intval($base) % 97;
     $rrn = new GeneratedRRN();
     $rrn->rrn = $base . str_pad($remainder, 2, '0', STR_PAD_LEFT);
-    $rrn->birthDate = "$day/$month/$year";
+    $rrn->birthDate = "19$year-$month-$day";
+    $rrn->gender = $seqNbr % 2 === 0 ? 'female' : 'male';
     return $rrn;
 }
 
