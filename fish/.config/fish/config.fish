ddf749eea1556eb0065f3276789daaa376b44c9b
Philippe Schottey <schophil@icloud.com>
2022-12-22 09:27:58 +0100

    feat: update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,7 +2,7 @@ set -x LANG "en_US.UTF-8"
 set -x LC_ALL "en_US.UTF-8"
 set -x EDITOR vim
 
-set -x EXTTRA_PATHS ~/Library/Python/3.9/bin ~/.npm-global/bin /opt/local/bin /opt/local/sbin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ~/tools/node/bin ~/bin ~/tools/idea/bin ~/tools/kubectx ~/.local/bin ~/tools/jmeter/bin
+set -x EXTTRA_PATHS ~/.npm-global/bin "/Applications/Visual Studio Code.app/Contents/Resources/app/bin" ~/bin ~/.local/bin ~/tools/jmeter/bin
 
 # set -x JAVA_HOME "temp"
 
@@ -22,12 +22,7 @@ end
 set -x hydro_color_git "#D19A66"
 set -x hydro_color_pwd "#56B6C2"
 
-alias ll "exa -l"
-alias ls "exa -l"
-alias lt "exa -T"
-
 fish_vi_key_bindings
-compose_path
+#compose_path
 
 set -e PATH_ORG
-direnv hook fish | source
