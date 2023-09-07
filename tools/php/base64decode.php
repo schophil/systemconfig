c7268c3f332bd8b16478051d22966ba21dbe34f7
Philippe Schottey <schophil@icloud.com>
2023-09-07 15:16:30 +0200

    feat: support special case token

--- tools/php/base64decode.php
+++ tools/php/base64decode.php
@@ -1,5 +1,23 @@
 <?php
 
-echo base64_decode($argv[1]);
+$value = null;
+$token = false;
 
+for ($i = 1; $i < sizeof($argv); $i++) {
+    $option = $argv[$i];
+    if ($option === '--token') {
+        $token = true;
+    } else if ($i == sizeof($argv) - 1) {
+        $value = $option;
+    }
+}
+
+if ($token) {
+    $value = str_replace('m__', '', $value);
+    $decoded = base64_decode($value);
+    exec("/usr/bin/google-chrome-stable https://jwt.io/#debugger-io?token=${decoded}");
+} else {
+    $decoded = base64_decode($value);
+    echo $decoded;
+}
 ?>
