092ed4eff218d345b84bcc0668bfa96341525fe6
Philippe Schottey <schophil@localhost.localdomain>
2024-01-09 14:16:13 +0100

    feat: bash config

---
+++ bash/custom/aliases.sh
@@ -0,0 +1,15 @@
+alias start-ssh-agent='eval $(ssh-agent -s)'
+alias afterwincopy='find . -type f -print0 | xargs -0 -I f chmod a-x,g+rw,o-w f'
+
+# git aliases
+alias branches='git -P branch'
+alias develop='git checkout develop'
+alias master='git checkout master'
+alias branch='git checkout -b'
+alias gitaivconfig='git config --local user.email "philippe.schottey@vlaanderen.be" && git config --local user.name "Philippe Schottey"'
+alias gitdefconfig='git config --local user.email "schophil@icloud.com" && git config --local user.name "Philippe Schottey"'
+
+# maven aliases
+alias mtree='./mvnw dependency:tree'
+alias minstall='./mvnw clean install'
+alias mtest='./mvnw clean install'
---
+++ bash/custom/mytask.sh
@@ -0,0 +1,7 @@
+mytask() {
+    local year=`date "+%Y"`
+    local day=`date "+%Y%m%d"`
+    local folder="${day}_$1"
+    mkdir -p "$year/$folder"
+    touch "$year/$folder/readme.md"
+}
