550e804b448a9e8209c0382947c57404b194e578
Philippe Schottey <schophil@icloud.com>
2024-11-04 11:38:08 +0100

    feat(bash): alias for api checks running

--- bash/custom/apichecks.sh
+++ bash/custom/apichecks.sh
@@ -1,4 +1,4 @@
-apicheckst() {
+apichecks() {
 	echo "newman run -e src/test/postman/$1.postman_environment.json src/test/postman/apichecks.postman_collection.json"
 	newman run -e "src/test/postman/$1.postman_environment.json" "src/test/postman/apichecks.postman_collection.json"
 }
