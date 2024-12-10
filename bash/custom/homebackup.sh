213aba8d2f6f49001c2610fea02a8d5e776cf388
Philippe Schottey <schophil@icloud.com>
2024-12-10 09:31:13 +0100

    feat(bash): simplify script

--- bash/custom/homebackup.sh
+++ bash/custom/homebackup.sh
@@ -1,22 +1,25 @@
 homebackup() {
-    if [[ -d /media/schophil/CCC1 ]]; then
-        echo "Detected backup device CCC1..."
-        backupTarget=/media/schophil/CCC1
-    elif [[ -d /media/schophil/CCC2 ]]; then
-        echo "Detected backup device CCC2..."
-        backupTarget=/media/schophil/CCC2
-    else
-        echo "No backup device detected!"
-        exit;
-    fi
+  devices=(/run/media/schophil/CCC1 /run/media/schophil/CCC2 /media/schophil/CCC1 /media/schophil/CCC2)
 
+  for i in ${devices[@]}; do
+    echo "Trying $i"
+    if [[ -d $i ]]; then
+      backupTarget=$i
+      break
+    fi
+  done
+
+  if [[ -z "$backupTarget" ]]; then
+    echo "No backup device detected!"
+  else
     echo "Backing up to $backupTarget"
     echo -n "Proceed? [Y/n]: "
     read -r answer
 
     if [[ "$answer" == "" || "$answer" == "y" ]]; then
-        rsync -a --stats --info=progress2 /home/schophil $backupTarget/Backups
+      rsync -a --stats --info=progress2 /home/schophil $backupTarget/Backups
     else
-        echo "Doing nothing"
+      echo "Doing nothing"
     fi
+  fi
 }
