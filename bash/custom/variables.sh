f975836c2b6be2eed786f4703c40551e6de79cdc
Philippe Schottey <schophil@icloud.com>
2025-06-14 08:57:13 +0200

    feat(bash): start ssh agent on wsl

--- bash/custom/variables.sh
+++ bash/custom/variables.sh
@@ -4,3 +4,13 @@ export NOTES_EDITOR NOTES_HOME EDITOR MAN_POSIXLY_CORRECT
 
 PATH=~/.local/bin:$PATH
 export PATH
+
+if uname -r | grep -q 'microsoft'; then
+	if [ -z "$SSH_AGENT_PID" ]; then
+		echo 'Starting ssh agent'
+		start-ssh-agent
+	else
+		echo 'SSH agent already running on ' $SSH_AGENT_PID
+	fi
+	#export BROWSER=/mnt/c/Program\ Files/Google/Chrome/Application/chrome.exe
+fi
