bc7b83c5f62f5df50bcc047a8057040c60ca9448
Philippe Schottey <phschotte@gmail.com>
2026-08-24 08:06:59 +0200

    feat(zsh): add aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -5,3 +5,9 @@ alias mvntree='mvn dependency:tree'
 alias develop='git checkout develop'
 alias master='git checkout master'
 alias branch='git checkout -b'
+alias unstage='git restore --staged .'
+
+# kubectl
+alias k='kubectl'
+
+alias ee='/mnt/c/Users/pschotte/Apps/sublime_text_build_4200_x64/sublime_text.exe'
